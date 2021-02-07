317f659fa9526808e7587cec6f9d94c40a3d99b9
Piotrek Kochański <khnsky@khnsky.cc>
2021-02-08 00:08:36 +0100

    add connect function

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,38 @@ class barebar {
     // exit.  we can however hide them inside a class and initialize and access
     // them using member functions.
     static struct {
+        xcb_connection_t* connection;
+        xcb_screen_t*     screen;
     } _;
 
+    static auto disconnect() noexcept {
+        xcb_disconnect(_.connection);
+    }
+
+    // TODO should there be check so that this can be only run once?
+    static auto connect(char const* display = nullptr) noexcept {
+        auto n = 0;
+        _.connection = xcb_connect(display, &n);
+
+        if (xcb_connection_has_error(_.connection))
+            die("[%s:%d] xcb_connect failed\n", __FILE__, __LINE__);
+
+        std::atexit(disconnect);
+
+        auto r = xcb_setup_roots_iterator(xcb_get_setup(_.connection));
+        for (auto i = 0; i != n; ++i)
+            xcb_screen_next(&r);
+
+        _.screen = r.data;
+
+        return _.connection;
+    }
+
 public:
-    static auto run() noexcept {}
+    static auto run() noexcept {
+        auto connection = connect();
+        (void)connection;
+    }
 };
 decltype(barebar::_) barebar::_;
 
