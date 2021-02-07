77918837dd06fa90781ed55ac9f8917e7d9dbdbf
Piotrek Kochański <khnsky@khnsky.cc>
2021-02-07 23:54:46 +0100

    add some utility functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,24 @@
 #include <xcb/randr.h>
 #include <xcb/xcb.h>
 
+// enclose everything in a anonymous namespace for that sweet internal linkage.
+namespace {
+
+template<class... Args>
+[[noreturn]] auto die(const char* fmt, Args... args) {
+    // I would use <format> but it is not yet implemented in std libs.
+    std::fprintf(stderr, fmt, args...);
+    exit(EXIT_FAILURE);
+}
+
+template<class T, class... Args>
+auto must(T&& t, char const* fmt, Args... args) {
+    if (!t)
+        die(fmt, args...);
+    return t;
+}
+
+}
+
 auto main() -> int {
 }
