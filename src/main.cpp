0edcbdfb666f4329402d818a2291a073fb8900a6
Piotrek Kochański <khnsky@khnsky.cc>
2021-02-09 20:48:13 +0100

    add function setting ewmh atoms

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
-#include <err.h>
-#include <xcb/randr.h>
+
 #include <xcb/xcb.h>
+
+#include <xcb/randr.h>
+#include <xcb/xcb_ewmh.h>
+
+#include <array>
+#include <string_view>
+using namespace std::literals;
 
 // enclose everything in a anonymous namespace for that sweet internal linkage.
 namespace {
@@ -137,12 +143,94 @@ class barebar {
         return _.window;
     }
 
+    static auto set_ewmh_atoms(xcb_connection_t* connection,
+                               xcb_window_t window,
+                               monitor output) noexcept {
+        auto names = std::array {
+            "_NET_WM_WINDOW_TYPE"sv,
+            "_NET_WM_WINDOW_TYPE_DOCK"sv,
+            "_NET_WM_DESKTOP"sv,
+            "_NET_WM_STRUT_PARTIAL"sv,
+            "_NET_WM_STRUT"sv,
+            "_NET_WM_STATE"sv,
+            "_NET_WM_STATE_STICKY"sv,
+            "_NET_WM_STATE_ABOVE"sv,
+        };
+
+        std::array<xcb_intern_atom_cookie_t, std::size(names)> cookies;
+        for (auto i = 0; i != std::size(names); ++i)
+            cookies[i] = xcb_intern_atom(connection,
+                                         false,
+                                         names[i].size(),
+                                         names[i].data());
+
+        std::array<xcb_atom_t, std::size(names)> atoms;
+        for (auto i = 0; i != std::size(names); ++i) {
+            auto reply = xcb_intern_atom_reply(connection, cookies[i], nullptr);
+            must(reply, "%s\n", "failed to get intern atom reply");
+
+            atoms[i] = reply->atom;
+            free(reply);
+        }
+
+        // left, right, top, bottom,
+        // left_start_y, left_endy, right_starty, right_end_y,
+        // top_start_x, top_end_x, bottom_start_x, bottom_end_x
+        auto strut = std::array<uint32_t, 12> {};
+        strut[2] = 50;                              // top
+        strut[8] = output.x;                        // top_start_x
+        strut[9] = output.x + output.w - 1;         // top_end_x
+
+        xcb_change_property(connection,
+                            XCB_PROP_MODE_REPLACE,
+                            window,
+                            atoms[0],
+                            XCB_ATOM_ATOM,
+                            32,
+                            1,
+                            &atoms[1]);
+        xcb_change_property(connection,
+                            XCB_PROP_MODE_APPEND,
+                            window,
+                            atoms[5],
+                            XCB_ATOM_ATOM,
+                            32,
+                            2,
+                            &atoms[6]);
+        xcb_change_property(connection,
+                            XCB_PROP_MODE_REPLACE,
+                            window,
+                            atoms[2],
+                            XCB_ATOM_CARDINAL,
+                            32,
+                            1,
+                            (uint32_t []) { 1 });
+        xcb_change_property(connection,
+                            XCB_PROP_MODE_REPLACE,
+                            window,
+                            atoms[3],
+                            XCB_ATOM_CARDINAL,
+                            32,
+                            12,
+                            strut.data());
+        xcb_change_property(connection,
+                            XCB_PROP_MODE_REPLACE,
+                            window,
+                            XCB_ATOM_WM_NAME,
+                            XCB_ATOM_STRING,
+                            8,
+                            7,
+                            "barebar");
+    }
+
 public:
     static auto run() noexcept {
         auto connection = connect();
         auto screen     = screen_of(connection);
         auto primary    = primary_output(connection, screen);
         auto window     = create_window(connection, screen, primary);
+
+        set_ewmh_atoms(connection, window, primary);
     }
 };
 decltype(barebar::_) barebar::_;
